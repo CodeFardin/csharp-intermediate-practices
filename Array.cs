8711cf3b88b85e36ccb088917129a9b62fd3cc7b
CodeFardin <fardinhasansheam999@gmail.com>
2026-04-08 22:12:26 +0600

    first commit

---
+++ Array.cs
@@ -0,0 +1,10 @@
+var arr = new int [7] { 2, 3, 4, 6, 5, 8, 9};
+int sum = 0;
+foreach ( int arr1 in arr)
+{   
+    sum = sum + arr1;
+}
+System.Console.WriteLine(sum + " ");
+
+System.Console.WriteLine(arr.Length);
+
